f4c74536780a82ea6d4b6d039f81af931328c9ca
Rayhan Billah <devilzhut@gmail.com>
2024-02-14 18:06:41 +0600

    use hhtps for goofle tiles

--- src/component/Map/index.js
+++ src/component/Map/index.js
@@ -62,7 +62,7 @@ const NetworkMap = () => {
       <MapContainer center={center} zoom={13} ref={setMap}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+          url="https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
           subdomains={["mt0", "mt1", "mt2", "mt3"]}
         />
         {/* <TileLayer
